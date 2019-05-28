737e14ff3add2372af80c371bba2e49fe02a9f00
luow <429175116@qq.com>
2019-05-28 17:55:28 +0800

    element ui导入

--- webapp/src/views/Transaction.vue
+++ webapp/src/views/Transaction.vue
@@ -2,10 +2,12 @@
 <template>
   <div class="transaction">
     <div class="nav">
-      <ul>
+      <!-- <ul>
         <li>
-          <i class="iconfont icon-shouye"></i>
-          营销中心
+          <router-link to="/marketing">
+            <i class="iconfont icon-shouye"></i>
+            营销中心
+          </router-link>
         </li>
         <li>
           <i class="iconfont icon-shouye"></i>
@@ -39,7 +41,89 @@
           <i class="iconfont icon-shouye"></i>
           营销中心
         </li>
-      </ul>
+      </ul> -->
+      <el-row class="tac">
+  <el-col>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+  <!-- <el-col :span="12">
+    <h5>自定义颜色</h5>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col> -->
+</el-row>
     </div>
     <div class="con">
       <div class="charts">
@@ -428,7 +512,16 @@ export default {
   name: 'transaction',
   components: {
     
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
+  
 }
 </script>
 <style scoped lang="less">
@@ -441,22 +534,22 @@ export default {
   width: 7.5rem;
   height: 100%;
   background-color: #fff;
-  li {
-    width: 5.9rem;
-    padding: 0.8rem;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    color: #484848;
-    font-size: 0.4rem;
-    .iconfont {
-      margin-right: 0.25rem;
-    }
-  }
-  li:hover {
-    background-color: #eff6ff;
-    color: #3889fc;
-  }
+  // li {
+  //   width: 5.9rem;
+  //   padding: 0.8rem;
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: flex-start;
+  //   color: #484848;
+  //   font-size: 0.4rem;
+  //   .iconfont {
+  //     margin-right: 0.25rem;
+  //   }
+  // }
+  // li:hover {
+  //   background-color: #eff6ff;
+  //   color: #3889fc;
+  // }
 }
 .con {
   width: 85.65rem;
