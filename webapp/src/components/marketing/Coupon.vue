11b7c0b02fd138aa5a3ff9b9b236eff39a3747cc
luow <429175116@qq.com>
2019-07-10 09:42:20 +0800

    优惠券布局完成

--- webapp/src/components/marketing/Coupon.vue
+++ webapp/src/components/marketing/Coupon.vue
@@ -1,6 +1,94 @@
 <template>
   <div class="con">
-    优惠券
+    <div class="charts">
+      <h3 class="title">优惠券</h3>
+      <div class="conter">
+        <div class="selType">
+          <div class="item">
+            <div>全部</div>
+            <div>未生效</div>
+            <div>已生效</div>
+            <div>已失效</div>
+          </div>
+        </div>
+        <div class="selType">
+          <div class="item">
+            <label>类型:</label>
+            <div>浏览数</div>
+            <div>浏览数</div>
+            <div>浏览数</div>
+          </div>
+          <div class="item">
+            <label>时间:</label>
+            <div>浏览数</div>
+            <div>浏览数</div>
+            <div class="active">浏览数</div>
+          </div>
+        </div>
+      </div>
+      <div class="conter">
+        <el-table
+          :data="tableData"
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="name"
+            label="名称"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="range"
+            label="有效范围"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="condition"
+            label="生效条件">
+          </el-table-column>
+          <el-table-column
+            prop="preferential"
+            label="优惠内容">
+          </el-table-column>
+          <el-table-column
+            prop="stock"
+            label="库存总量">
+            <!-- <template slot-scope="scope">
+              <el-button @click="handleClick(scope.row)" type="text" size="small">派发</el-button>
+            </template> -->
+          </el-table-column>
+          <el-table-column
+            prop="status"
+            label="状态">
+          </el-table-column>
+          <el-table-column
+            prop="receive"
+            label="领取状态">
+            <template slot-scope="scope">
+              <el-switch
+                v-model="scope.row.receive"
+                active-text=""
+                inactive-text="">
+              </el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="right"
+            label="管理">
+            <template slot-scope="scope">
+              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="conter">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="1000">
+        </el-pagination>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,7 +101,35 @@ export default {
   data() {
     return {
       domList: [],
-      isOpenWind: true //点击X关闭窗口
+      isOpenWind: true, //点击X关闭窗口
+      tableData: [{
+          name: '2016-05-02',
+          range: '王小虎',
+          condition: '上海市普陀区金沙江路 1518 弄',
+          preferential: '王小虎',
+          stock: '王小虎',
+          status: '王小虎',
+          receive: true,
+          right: '王小虎'
+        },{
+          name: '2016-05-02',
+          range: '王小虎',
+          condition: '上海市普陀区金沙江路 1518 弄',
+          preferential: '王小虎',
+          stock: '王小虎',
+          status: '王小虎',
+          receive: false,
+          right: '王小虎'
+        },{
+          name: '2016-05-02',
+          range: '王小虎',
+          condition: '上海市普陀区金沙江路 1518 弄',
+          preferential: '王小虎',
+          stock: '王小虎',
+          status: '王小虎',
+          receive: true,
+          right: '王小虎'
+        }]
     };
   },
   mounted() {},
@@ -28,5 +144,73 @@ export default {
   display: inline-flex;
   flex-wrap: wrap;
 }
+.charts,
+.list {
+  width: 93rem;
+  height: auto;
+  margin-top: 0.5rem;
+  background-color: #fff;
+  border-radius: 0.5rem;
+  .title {
+    padding: 1rem;
+    font-weight: 600;
+    font-size: 0.75rem;
+    color: #343434;
+    border-bottom: 1px solid #f7f7f7;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-size: 0.65rem;
+      color: #3889fc;
+      margin-left: 1rem;
+    }
+  }
+  .conter {
+    width: 91rem;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // flex-wrap: wrap;
+    .selType {
+      // width: 100%;
+      display: flex;
+      align-items: center;
+      // justify-content: flex-start;
+      .item {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin-right: 1rem;
+        label {
+          font-size: 0.75rem;
+          color: #5a5a5a;
+          font-weight: 600;
+        }
+        div {
+          font-size: 0.75rem;
+          padding: 0.3rem 0.75rem;
+          border: 1px solid #c4c4c4;
+          color: #c4c4c4;
+          border-radius: 0.25rem;
+          margin: 0 0.5rem;
+        }
+        .active,
+        div:hover {
+          background-color: #3889fc;
+          border-color: #3889fc;
+          color: #fff;
+        }
+      }
+    }
+  }
+  .el-table .warning-row {
+    background: oldlace;
+  }
 
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+}
 </style>
