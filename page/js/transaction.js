a5a6b87eafb781295009b4079b606c98928c0c61
luow <429175116@qq.com>
2019-07-13 18:04:07 +0800

    -

--- page/js/transaction.js
+++ page/js/transaction.js
@@ -1,6 +1,46 @@
 new Vue({
   el: '#app',
   data: function () {
-    return { visible: false }
+    return { 
+      visible: false,
+      tableData: [
+        {
+          time: "昨日",
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          time: "7日",
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          time: "30日",
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          time: "历史汇总",
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          time: "每日平均",
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          time: "历史峰值",
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        }
+      ]
+     }
   }
 })
